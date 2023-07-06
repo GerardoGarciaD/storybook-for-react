6f253a4612158a5fc589d8fba267ebab5769befd
Gerardo <geraz26@hotmail.com>
2023-07-06 16:30:44 -0600

    Añadiendo diseños de figma a las stories

--- src/components/Badge/Badge.stories.tsx
+++ src/components/Badge/Badge.stories.tsx
@@ -8,6 +8,12 @@ export default {
   args: {
     text: 'Comfort food',
   },
+  parameters: {
+    design: {
+      type: 'figspec',
+      url: 'https://www.figma.com/file/3Q1HTCalD0lJnNvcMoEw1x/Mealdrop?type=design&node-id=1906-3469&mode=design&t=yDKGMKcGtHgM8jaG-4',
+    },
+  },
   //   Aqui se hace una referencia al componente que queremos testear y asi poder enviarle args (props) por default a todas las tories que se van a crear
 } as ComponentMeta<typeof Badge>
 
