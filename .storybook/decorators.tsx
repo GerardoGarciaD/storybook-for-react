0ac359a9084008ea47ddd0c7bb5d100c0e982209
Gerardo <geraz26@hotmail.com>
2023-07-06 15:30:44 -0600

    Añadiendo decorator para poder añadir estilos a las historias

---
+++ .storybook/decorators.tsx
@@ -0,0 +1,16 @@
+import { DecoratorFn } from "@storybook/react"
+import { GlobalStyle } from '../src/styles/GlobalStyle';
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import { lightTheme } from '../src/styles/theme';
+
+
+const withTheme: DecoratorFn = (StoryFn) =>(
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyle />
+      <StoryFn />
+    </ThemeProvider>
+  )
+
+  export const globalDecorators = [withTheme];
+
