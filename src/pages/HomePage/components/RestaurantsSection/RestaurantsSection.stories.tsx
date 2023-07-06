22c28c37d7947e05f7530ab1b288798d76dee79d
Gerardo <geraz26@hotmail.com>
2023-07-06 17:13:04 -0600

    Creando story de los restaurantes favoritos

---
+++ src/pages/HomePage/components/RestaurantsSection/RestaurantsSection.stories.tsx
@@ -0,0 +1,23 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
+import { RestaurantsSection } from './RestaurantsSection.container'
+
+export default {
+  title: 'Pages/HomePage/Components/RestaurantsSection',
+  component: RestaurantsSection,
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/3Q1HTCalD0lJnNvcMoEw1x/Mealdrop?type=design&node-id=135-311&mode=design&t=jtxNgXn8WvcshyDk-4',
+    },
+  },
+} as ComponentMeta<typeof RestaurantsSection>
+
+const Template: ComponentStory<typeof RestaurantsSection> = (args) => (
+  <RestaurantsSection {...args} />
+)
+
+export const Default = Template.bind({})
+Default.args = {
+  title: 'Our Favorite picks',
+}
